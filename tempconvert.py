0853c321f34e5a09b6d37008269758825a8db492
scrum9 <142543638+scrum9@users.noreply.github.com>
2023-08-21 05:14:04 -0500

    Add files via upload

---
+++ tempconvert.py
@@ -0,0 +1,12 @@
+# Temperature Converter
+# 8/31/2020
+# By: Conall Woolcott
+
+def function():
+    fahrenheit = int(input("Enter the temperature in F: "))
+    celsius = int(fahrenheit - 32)*5/9
+    print ("The temperature is", celsius, "degrees celsius.")
+function()
+
+
+
